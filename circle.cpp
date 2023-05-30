39eb0551f24cf561f979deae1bf2db518c9e1288
Sakshi Raut <sakshiraut374@gmail.com>
2023-05-31 05:09:20 +0530

    cpp file

---
+++ circle.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class base
+{
+    public:
+    float radius;
+    float pi;
+    virtual void fun(){
+        cout<<"inside base fun";
+    }
+};
+
+class derived:public base
+{
+    public:
+    virtual void fun(){
+        cout<<"inside derived fun"<<endl;
+    }
+};
+
+class derivedsecond:public derived
+{
+    
+};
+
+int main()
+{
+    derivedsecond obj1;
+    derived obj2;
+    base obj3;
+    base * dp =NULL; 
+    dp=&obj1;  
+    dp->fun();
+    cout<<"inside main"<<endl;
+    
+}
